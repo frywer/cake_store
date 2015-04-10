3c299317b3f49fa7550a19f0842a35aeb2594ff5
frywer <marangoz.frywer@gmail.com>
2015-04-10 23:50:08 +0200

    add rus errors 1 part

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: {message: 'Такой товар уже существует!'}
   validates :image_url, allow_blank: true, format: {
                           with: %r{\.(gif|jpg|png)\Z}i,
                           message: 'URL должен указывать на изображение формата GIF, JPG или PNG.'
