6cc5d211670862b9d214d2a509895ae476374253
frywer <marangoz.frywer@gmail.com>
2015-04-11 00:08:52 +0200

    finish validation config

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  validates :title, :description, :image_url, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, :description, :image_url, presence: {message: 'Поле должно быть заполнено!'}
+  validates :price, numericality: {greater_than_or_equal_to: 0.01, message: 'Цена отсутствует!'}
   validates :title, uniqueness: {message: 'Такой товар уже существует!'}
   validates :image_url, allow_blank: true, format: {
                           with: %r{\.(gif|jpg|png)\Z}i,
