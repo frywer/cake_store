f7bb7309842223b8a5aefd3eea7f1b8f4046f1f6
frywer <marangoz.frywer@gmail.com>
2015-04-20 20:41:29 +0200

    Add cart part 1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: {message: 'Поле должно быть заполнено!'}
   validates :price, numericality: {greater_than_or_equal_to: 0.01, message: 'Цена отсутствует!'}
   validates :title, uniqueness: {message: 'Такой товар уже существует!'}
@@ -9,5 +13,17 @@ class Product < ActiveRecord::Base
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  private
+  # убеждаемся в отсутствии товарных позиций, ссылающихся на данный товар
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+       true
+    else
+      errors.add(:base, 'существуют товарные позиции')
+      false
+    end
   end
 end
