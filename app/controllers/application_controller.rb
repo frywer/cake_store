b0ddf8a54fb87216b033428aeb3bc9bee9b3f15f
frywer <marangoz.frywer@gmail.com>
2015-05-11 10:54:07 +0200

    Add administrator validations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  protected
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+    end
+  end
 end
