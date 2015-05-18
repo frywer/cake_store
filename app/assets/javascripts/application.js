aefc7db4a57169d41b303d1ba56d13b1e78112e2
frywer <marangoz.frywer@gmail.com>
2015-05-18 12:45:06 +0200

    header

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,8 @@
 //= require jquery
 //= require jquery-ui/effect-blind
 //= require jquery_ujs
+//= require foundation
 //= require turbolinks
 //= require_tree .
+
+$(function(){ $(document).foundation(); });
