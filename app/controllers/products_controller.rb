24073da625724fd1bb116b23ca157c104d415744
frywer <marangoz.frywer@gmail.com>
2015-07-29 02:37:07 +0200

    fdasfsa

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
+    @products = Product.all.page(params[:page]).per_page(8)
   end
 
   # GET /products/new
